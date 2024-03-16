ad83dadaab28ff5a9e8abf370297eb0fda8e0d78
Naol Lemessa <131991870+naol16@users.noreply.github.com>
2024-03-16 15:54:10 +0300

    Time: 2369 ms (100.00%), Space: 21.2 MB (100.00%) - LeetHub

---
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.py
@@ -0,0 +1,23 @@
+from collections import Counter
+class Solution(object):
+    def maxScoreIndices(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        score = max_score = sum(nums)
+        highest_scores = [0]
+        
+        for i, v in enumerate(nums, 1):
+            score += 1 if v == 0 else -1
+            if score > max_score:
+                highest_scores = [i]
+                max_score = score
+            elif score == max_score:
+                highest_scores.append(i)
+        
+        return highest_scores
+       
+
+
+             
