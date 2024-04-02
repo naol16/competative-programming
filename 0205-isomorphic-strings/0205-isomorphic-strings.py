036ddeab07af8cda500fa7fddd6faa34c89a2ca0
Naol Lemessa <131991870+naol16@users.noreply.github.com>
2024-04-03 00:00:39 +0300

    Time: 19 ms (85.13%), Space: 14.7 MB (13.35%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,11 @@
+from collections import Counter 
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        return len(set(zip(s,t))) == len(set(s)) == len(set(t))
+        
