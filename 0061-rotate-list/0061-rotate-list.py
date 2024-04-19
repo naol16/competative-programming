ca413fe74b8a15f1b8dfd1fa673c1b8dab2897d1
Naol Lemessa <131991870+naol16@users.noreply.github.com>
2024-04-19 10:45:02 +0300

    Time: 18 ms (69.40%), Space: 11.7 MB (33.47%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -3,9 +3,42 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next           
+# class Solution(object):
+#     def rotateRight(self, head, k):
+#         """
+#         :type head: ListNode
+#         :type k: int
+#         :rtype: ListNode
+#         """
+#         current=head
+#         num=1
+#         prev=None
+        
+#         length=1
+#         if not head:
+#           return None
+        
+#         while current.next:
+              
+#               current=current.next
+#               length+=1
+#         k=k%length
+#         if k==0:
+#            return head
+#         last_element=current
+#         current=head
+       
+#         left=head
+#         for  i in range(0,length-k-1):
+#              left=left.next
+              
+#         right=left.next
+#         left.next=None
+#         last_element.next=head
+#         return right
 class Solution(object):
     def rotateRight(self, head, k):
-        """
+        """"
         :type head: ListNode
         :type k: int
         :rtype: ListNode
@@ -14,27 +47,28 @@ class Solution(object):
         num=1
         prev=None
         
-        length=1
+        length=0
         if not head:
           return None
         
-        while current.next:
-              
+        while current:
+              prev=current
               current=current.next
               length+=1
         k=k%length
-        if k==0:
-           return head
-        last_element=current
+        last_element=prev
         current=head
        
         left=head
-        for  i in range(0,length-k-1):
+        for  i in range(1,length-k):
              left=left.next
-              
+        if k==0:
+           return head
         right=left.next
         left.next=None
         last_element.next=head
         return right
+            
+              
             
                             
