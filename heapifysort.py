45748f04b23b42644b709aede637b65758586727
Naol Lemessa <131991870+naol16@users.noreply.github.com>
2024-10-08 22:17:56 +0300

    Create heapifysort.py

---
+++ heapifysort.py
@@ -0,0 +1,14 @@
+largest = i 
+        left = 2 * i + 1 
+        right = 2 * i + 2 
+
+        if left < n and arr[left] > arr[largest]:
+            largest = left
+
+        if right < n and arr[right] > arr[largest]:
+            largest = right
+
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i] 
+
+            self.heapify(arr, n, largest)
